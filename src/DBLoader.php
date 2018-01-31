132e264822084cb99b310bb6c49ec84640e2979a
Eduard Duocastella Altimira <eduard.duocastella@gmail.com>
2018-01-31 14:42:22 +0100

    fixed process showing output

--- src/DBLoader.php
+++ src/DBLoader.php
@@ -1,6 +1,5 @@
 <?php namespace RevoSystems\DBSync;
 
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use File;
 use Symfony\Component\Process\Process;
 
@@ -9,19 +8,11 @@ class DBLoader {
     public static function loadFile($dbUsername, $dbPassword, $databaseFile,$revo_db) {
         $command = "mysql --user=".$dbUsername." --password=".$dbPassword. " $revo_db < $databaseFile";
         $process = new Process($command);
-        return $process->run();
-    }
- 
-    public static function readFile($filename) {
-        try
-        {
-            $contents = File::get($filename);
-            return $contents;
-        }
-        catch (FileNotFoundException $exception)
-        {
-            return null;
-        }
-    }
+        $process->run();
     
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+        return $process->getOutput();
+    }
 }
