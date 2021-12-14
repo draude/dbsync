18a46219f41e2c3986ac4f49c26eea574ef54012
Eduard Duocastella <eduard@revo.works>
2021-12-15 00:28:37 +0100

    fixed process

--- src/DBLoader.php
+++ src/DBLoader.php
@@ -1,20 +1,18 @@
 <?php namespace RevoSystems\DBSync;
 
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-
 class DBLoader {
     
     public static function loadFile($dbUsername, $dbPassword, $databaseFile,$revo_db,$dbHost = "") {
-        if ($dbHost !== "")
+        if ($dbHost !== "") {
             $dbHost = "--host=$dbHost";
-        $command = "mysql $dbHost --user=".$dbUsername." --password=".$dbPassword. " $revo_db < $databaseFile";
-        $process = new Process($command);
-        $process->run();
-    
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
         }
-        return $process->getOutput();
+
+        exec("mysql $dbHost --user=$dbUsername --password=$dbPassword $revo_db < $databaseFile 2>&1",$retArr,$output);
+
+        if (intval($output) > 0) {
+            throw new \Exception("SQL import Failed");
+        }
+
+        return "OK";
     }
 }
